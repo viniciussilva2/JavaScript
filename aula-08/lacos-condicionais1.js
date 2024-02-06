8d285f3b3d7f4caba58bff3068e541d7ba4c6eaf
Vinicius Silva <vinyoli.vs@gmail.com>
2024-02-06 20:24:07 -0300

    Update lacos-condicionais1.js
    Média dos alunos

--- aula-08/lacos-condicionais1.js
+++ aula-08/lacos-condicionais1.js
@@ -2,12 +2,33 @@
 
 const prompt = require("readline-sync");
 
-let saldo = Number(prompt.question("Qual eh o seu saldo ?"));
+//let saldo = Number(prompt.question("Qual eh o seu saldo ?"));
 
-while (saldo < 0) {
-  saldo = Number(
-    prompt.question("Saldo inva-lido! Por favor, informe novamente!!")
-  );
+//while (saldo < 0) {
+//saldo = Number(
+//  prompt.question("Saldo inva-lido! Por favor, informe novamente!!")
+// );
+//}
+
+//console.log(saldo);
+
+// Exemplo 2
+
+let notaDoAluno = Number(prompt.question("Informe a nota do aluno: "));
+
+let somaDasNotas = 0; // Acumulador
+// 10 > 10
+// 8 > 18
+
+let qtdNotasValidas = 0; // Acumulador (Quantidade de notas válidas digitadas)
+
+while (notaDoAluno >= 5) {
+  somaDasNotas = somaDasNotas + notaDoAluno; //Incrementando o acumulador que é a soma das notas.
+  notaDoAluno = Number(prompt.question("Informe a próxima nota: "));
+  qtdNotasValidas = qtdNotasValidas + 1;
 }
 
-console.log(saldo);
+console.log(somaDasNotas);
+console.log(qtdNotasValidas);
+
+console.log("Média das notas dos alunos", somaDasNotas / qtdNotasValidas);
